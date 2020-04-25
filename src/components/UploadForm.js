65a3155ad1221ca217e0c2c3df1325de2132b8c7
tamaszentai <zentaitamas@gmail.com>
2020-04-25 12:45:48 +0100

    form works

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,13 +1,40 @@
 import React, { useState } from 'react';
 
 const UploadForm = () => {
+    const [values, setValues] = useState({name: '', file: null, description: ''})
+
+    const handleInputChange = e => {
+        const {name, value} = e.target
+        setValues({...values, [name]: value})
+    }
+
     return (
         <div className="uploadform">
             <form>
-                <input type="text" placeholder="File name" />
-                <input type="file" />
-                <textarea placeholder="Description here..." rows="10" cols="30"></textarea>
-                <button type="submit" value="">Upload</button>
+                <input 
+                name='name'
+                type="text" 
+                placeholder="File name" 
+                value={values.name}
+                onChange={handleInputChange}/>
+
+                <input 
+                name='file'
+                type="file" 
+                value={values.file}
+                onChange={handleInputChange}/>
+
+                <textarea 
+                name='description'
+                placeholder="Description here..." 
+                rows="10" 
+                cols="30" 
+                value={values.description} 
+                onChange={handleInputChange}/>
+
+                <input
+                type="submit" 
+                value="Upload" />
             </form>
         </div>
     )
