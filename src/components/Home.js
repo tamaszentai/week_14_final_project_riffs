79cb0c8d459bccb5b656d8e45c766634c2d1ff91
tamaszentai <zentaitamas@gmail.com>
2020-04-26 15:49:51 +0100

    lorem ipsum added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import React from 'react';
 const Home = () => {
     return (
         <>
-        HOMEPAGE
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fermentum elit eu ullamcorper lobortis. Nulla facilisi. Suspendisse non augue vitae est viverra faucibus. Pellentesque malesuada pharetra mauris, et rutrum felis porttitor quis. Nulla non feugiat velit. Pellentesque non metus et odio vulputate luctus facilisis sagittis metus. Praesent sed leo vitae ex maximus auctor a eu tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin iaculis vel urna quis elementum.
+
+Etiam non augue eget eros faucibus ultricies. Quisque vulputate enim ac tristique finibus. Pellentesque imperdiet sodales efficitur. Nullam fermentum sem non pharetra tempor. Aliquam eget neque a elit mattis tempor at ut lorem. Vivamus sed scelerisque mi. Donec luctus dui in neque molestie, in fermentum metus ullamcorper. Praesent vitae nisi ut leo condimentum posuere. In sodales diam nisl, vel posuere lorem accumsan vel. Integer gravida laoreet orci eget imperdiet. In imperdiet nunc et eros venenatis, eget condimentum neque bibendum. Cras ac arcu eu justo cursus sodales non ac odio. Aenean eu diam quis risus vehicula elementum. Aliquam sed condimentum neque, nec pretium orci. Cras faucibus porta nisi vitae dignissim. Nulla malesuada sapien vitae odio viverra ultrices.
+
+Nullam et nisi dapibus, blandit tortor ac, condimentum libero. Nunc elementum tellus ut ligula vehicula, eu ornare ligula ornare. Fusce pulvinar leo nec euismod vehicula. Integer faucibus ex ac ipsum ullamcorper consequat. Nam euismod sodales ex et pharetra. Nulla et vestibulum sapien. Curabitur porttitor erat ipsum, eu egestas eros efficitur eu. Ut dignissim, sem at convallis convallis, est elit fermentum risus, at commodo quam enim vitae lectus. Nullam nec sem id arcu placerat finibus. Curabitur aliquam volutpat odio non lacinia.
+
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam vitae laoreet ante, sed pellentesque dolor. Etiam iaculis lorem ex. In scelerisque aliquam suscipit. Sed molestie odio vitae lectus pellentesque, quis eleifend arcu dapibus. Praesent non elit commodo, finibus urna sed, posuere mauris. Integer ultrices sem risus, feugiat dapibus eros volutpat ac.
+
+Sed quis est interdum, placerat tortor vel, laoreet erat. Suspendisse potenti. Integer facilisis odio vel semper interdum. Aenean vel justo nec purus iaculis pellentesque eget id sem. Nulla iaculis vehicula dictum. Nullam justo turpis, commodo et sollicitudin eu, ornare a magna. Sed laoreet eu felis sit amet pretium. Nulla eu mi a urna placerat sollicitudin a id dolor. Phasellus molestie justo at lectus aliquet cursus. Donec accumsan, eros nec semper convallis, dui eros ullamcorper erat, vel posuere ipsum dolor non mauris. Aenean semper justo non lacinia elementum. Fusce dapibus nulla ac orci auctor congue.
         </>
     )
 }
